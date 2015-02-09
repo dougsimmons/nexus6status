ee973940400251f6a1ab816780b8f211f706ebbd
Doug Simmons <etc@302.nyc>
2015-02-09 12:20:51 -0500

    Update nexus6status.sh

--- nexus6status.sh
+++ nexus6status.sh
@@ -3,18 +3,21 @@
 ## Tune as needed and add to cron, perhaps as @hourly [scriptpath]  >/dev/null
 ## Author: Doug Simmons https://google.com/+DougSimmonsIT
 ## Github: https://github.com/dougsimmons/nexus6status/
-## License: Do whatever you want obviously... just don't take my nexus 6! ;)                 
   
-PLAYURL="https://play.google.com/store/devices/details/Nexus_6_64GB_Midnight_Blue?id=nexus_6_blue_64gb"
-NOINVENTORYTEXT="out of inventory"
-
-ALERTSUBJECT="Nexus 6 online maybe?"
-ALERTBODY="Go on Google Play"
-
-MAILFROM="alertaddress@asdf.com"
-ALERTEMAIL="YOU@asdf.com"
-
-WGETPATH=/usr/bin/wget
-MAILXPATH=/usr/bin/mailx
-
-$WGETPATH $PLAYURL --timeout 30 -O - 2>/dev/null | grep "$NOINVENTORYTEXT" || echo "$ALERTBODY" "$PLAYURL" | $MAILXPATH -v -s "$ALERTSUBJECT" -r $MAILFROM $ALERTEMAIL
+PLAYURL="https://play.google.com/store/devices/details/Nexus_6_64GB_Midnight_Blue?id=nexus_6_blue_64gb" 
+NOINVENTORYTEXT="out of inventory" 
+ALERTSUBJECT="Nexus 6 online maybe?!" 
+ALERTBODY="Go on Google Play" 
+ 
+MAILFROM="FromAddress@asdf.com" 
+ALERTEMAIL="YOU@asdf.com" 
+ 
+CURLPATH=/usr/bin/curl 
+MAILXPATH=/usr/bin/mailx 
+NOW=$(date +"%T %m-%d") 
+ 
+$CURLPATH --connect-timeout 20 -s $PLAYURL | grep -i "$NOINVENTORYTEXT" 
+if [ "$?" -ne "0" ]; 
+        then  
+                echo "$ALERTBODY \n $PLAYURL \n \n timestamp $NOW" | $MAILXPATH -v -s "$ALERTSUBJECT" -r $MAILFROM $ALERTEMAIL 
+fi 
